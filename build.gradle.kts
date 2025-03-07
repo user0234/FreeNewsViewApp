491d7afaaacafba0d8823c723db224e62b9add9c
user1111 <harnammail454545@gmail.com>
2025-03-07 12:13:44 +0530

    test commit small change

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript{
 
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath ("io.realm:realm-gradle-plugin:10.11.1")
         kotlin("plugin.serialization")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
